49e64734980931678f2f7e841d89da426733ea7d
js <shk3029>
2019-01-19 17:05:02 +0900

    intial commit

---
+++ src/main/java/me/js/jongseon/JongseonApplication.java
@@ -0,0 +1,14 @@
+package me.js.jongseon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JongseonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JongseonApplication.class, args);
+    }
+
+}
+
