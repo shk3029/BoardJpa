ae5d579c002b31e2bea27a2ffb52dbfd71e83612
js <shk3029>
2019-01-19 20:24:10 +0900

    README 추가

--- src/main/java/me/js/jongseon/JongseonApplication.java
+++ src/main/java/me/js/jongseon/JongseonApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JongseonApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(JongseonApplication.class, args);
+
+
     }
 
 }
